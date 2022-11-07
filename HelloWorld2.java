4525f99f53a064f075d5c9cc5e083af1ce0dc6ef
yogayataverma <63913693+yogayataverma@users.noreply.github.com>
2022-11-07 11:37:08 +0530

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,7 @@
+// Your First Program
+
+class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World2!"); 
+    }
+}
---
+++ HelloWorld3.java
@@ -0,0 +1,7 @@
+// Your First Program
+
+class HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World3!"); 
+    }
+}
